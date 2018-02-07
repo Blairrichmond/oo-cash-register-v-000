ad18c60bb5bc78b457c4dd1ffb73d3fd2ec8efd4
Brent  Ransom <sadumedusa@gmail.com>
2018-02-07 22:02:42 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,6 +10,8 @@ class CashRegister
 
   def add_item(item, price, amount = 1)
     @total += price * amount
+    @last_transaction = price * amount
+    amount.times {@item << item}
   end
 
   def apply_discount
