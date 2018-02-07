3670009138fd8a6ef639d3a981f7d57e26b3c031
Brent  Ransom <sadumedusa@gmail.com>
2018-02-07 21:51:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
 
-  attr_accessor :total, :item
+  attr_accessor :total, :item, :discount
 
   def initialize(discount = 0)
     @total = 0
+    @discount = discount
     @item = item
   end
 end
